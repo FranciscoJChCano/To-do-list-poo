28e5d4c2a813165a9bd1aa0b02eea67f194fe00d
FranciscoJChCano <franciscojcharnecocano@gmail.com>
2024-06-10 11:17:05 +0200

    create class

---
+++ src/main/java/com/example/Task.java
@@ -0,0 +1,28 @@
+package com.example;
+
+public class Task {
+    private String description;
+    private boolean completed;
+  
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+    }
+  
+    public String getDescription() {
+        return description;
+    }
+  
+    public boolean isCompleted() {
+        return completed;
+    }
+  
+    public void complete() {
+        this.completed = true;
+    }
+  
+    
+    public String toString() {
+        return (completed ? "[X] " : "[ ] ") + description;
+    }
+  }
